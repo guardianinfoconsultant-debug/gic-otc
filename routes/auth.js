8aac2c4df3e08038a5eef4f0f3872bd2ac52675c
Kamal <guardianinfoconsultant@gmail.com>
2026-02-14 16:13:43 +0530

    Fix auth.js model case issue

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const User = require("../models/user");
 
 router.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
