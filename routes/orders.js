600a2af5cdd7d65fe006028407ec14291b6b72e6
guardianinfoconsultant-debug <guardianinfoconsultant@gmail.com>
2026-02-14 15:40:23 +0530

    Update orders.js

--- routes/orders.js
+++ routes/orders.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Order = require("../models/Order");
+const Order = require("../models/order");
 const User = require("../models/User");
 const Price = require("../models/Price");
 const Log = require("../models/Log");
